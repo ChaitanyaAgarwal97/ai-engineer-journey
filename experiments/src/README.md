6a3ca7885979e09f326de758f5646e9334d5adc3
Chaitanya Agarwal <chaitanyaagarwal97@gmail.com>
2026-07-23 01:20:33 +0530

    added conversation memory

---
+++ experiments/src/README.md
@@ -0,0 +1,17 @@
+# Aether CLI
+
+A terminal-based AI chatbot built using TypeScript and the Gemini API.
+
+## Features
+
+- Interactive CLI
+- Multi-turn conversation
+- Conversation memory
+- Environment variables
+- Error handling
+
+## Run
+
+npm install
+
+npm run dev
--- experiments/src/chatbot.ts
+++ experiments/src/chatbot.ts
@@ -2,34 +2,55 @@ import Groq, { APIConnectionError, AuthenticationError } from "groq-sdk";
 import { askPrompt, closePrompt } from "./input.js";
 import { GROQ_API_KEY } from "./config.js";
 
+let history: { role: "user" | "assistant" | "system"; content: string }[] = [];
+
 export default async function () {
   const client = new Groq({ apiKey: GROQ_API_KEY });
 
   while (true) {
     console.log("\n-------------------------------------");
     const prompt = await askPrompt();
-    if (prompt.trim().toLowerCase() === "exit") {
-      break;
-    }
-    try {
-      const response = await client.chat.completions.create({
-        messages: [
-          {
-            role: "user",
-            content: prompt,
-          },
-        ],
-        model: "openai/gpt-oss-20b",
-      });
+    if (prompt) {
+      if (prompt.trim().toLowerCase() === "exit") {
+        break;
+      }
+      if (prompt.trim().toLowerCase() === "clear") {
+        history = [];
+        console.log("\nSystem:\nConversation history cleared.");
+        continue;
+      }
+      try {
+        const response = await client.chat.completions.create({
+          messages: [
+            ...history,
+            {
+              role: "user",
+              content: prompt,
+            },
+          ],
+          model: "openai/gpt-oss-20b",
+        });
 
-      console.log("\nAI:\n" + response.choices[0]?.message?.content);
-    } catch (e) {
-      if (e instanceof APIConnectionError) {
-        console.log(
-          "\nSystem:\nPlease check your internet connection and try again.",
-        );
-      } else if (e instanceof AuthenticationError) {
-        console.log("\nSystem:\nUnexpected error occurred.");
+        const responseMessage = response.choices[0]?.message?.content;
+        history.push({
+          role: "user",
+          content: prompt,
+        });
+        if (responseMessage) {
+          history.push({
+            role: "assistant",
+            content: responseMessage,
+          });
+        }
+        console.log("\nAI:\n" + responseMessage);
+      } catch (e) {
+        if (e instanceof APIConnectionError) {
+          console.log(
+            "\nSystem:\nPlease check your internet connection and try again.",
+          );
+        } else if (e instanceof AuthenticationError) {
+          console.log("\nSystem:\nUnexpected error occurred.");
+        }
       }
     }
   }
